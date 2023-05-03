42f0aa4f081401b1bf6085719eb533aef638c09f
bessavagner <bessavagner@gmail.com>
2023-05-03 06:05:17 -0300

    fix(Base): handle every TypeError in absolute_links

--- src/pyckaxe/base.py
+++ src/pyckaxe/base.py
@@ -11,78 +11,69 @@ from pyckaxe import CONFIG_LOG
 
 dictConfig(CONFIG_LOG)
 
-logger = logging.getLogger('standard')
+logger = logging.getLogger("standard")
+
 
 class BaseInspector:
-
     def __init__(self, url: str):
-
         self.session = None
         self.page = None
         self._soup = None
 
         if isinstance(url, str):
-
             self.url = url
 
         else:
-
             raise ValueError(f"Url must be string, {type(url)} was given")
 
     def find_all(self, *args, **kwargs):
-
         try:
-
             return self._soup.find_all(*args, **kwargs)
 
         except AttributeError:
-
-            if __class__.__name__ == 'BaseInspector':
-
+            if __class__.__name__ == "BaseInspector":
                 logger.warning(
-                    'Using an instance of %s. Returning None',
-                    __class__.__name__
+                    "Using an instance of %s. Returning None", __class__.__name__
                 )
-    
+
                 return None
 
-    def anchors(self, ):
+    def anchors(
+        self,
+    ):
+        return self.find_all("a")
 
-        return self.find_all('a')
-
-    def absolute_links(self, contains: str=None):
-
+    def absolute_links(self, contains: str = None):
         links = self.anchors()
         absolute_links_ = []
 
         try:
-
             for link in links:
-                relative_link = link.attrs['href']
+                relative_link = link.attrs["href"]
 
                 if contains is not None:
                     if contains not in relative_link:
                         continue
-    
+
                 url = urljoin(self.url, relative_link)
                 absolute_links_.append(url)
 
             return absolute_links_
 
         except TypeError as err:
+            if err == "'NoneType' object is not iterable":
+                if links is None:
+                    if __class__.__name__ == "BaseInspector":
+                        logger.warning(
+                            "Using an instance of %s. Returning empty list",
+                            __class__.__name__,
+                        )
 
-            if links is None:
-
-                if __class__.__name__ == 'BaseInspector':
-
-                    logger.warning(
-                        'Using an instance of %s. Returning empty list',
-                        __class__.__name__
-                    )
-
-                    return []
+                        return []
 
 
 class BaseHandleData:
-    def __init__(self,):
+    def __init__(
+        self,
+    ):
         pass
